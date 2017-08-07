f6618010ba06f4929080cafade56ef1f2bc62110
ljb48229 <ljb48229@ly.com>
2017-08-07 19:06:34 +0800

    项目demo

---
+++ AspectsDemo-master/AspectsDemo/GLLogging.h
@@ -0,0 +1,24 @@
+//
+//  LoggingConfig.h
+//  Test
+//
+//  Created by Peng Gu on 12/16/14.
+//  Copyright (c) 2014 Peng Gu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <Aspects.h>
+
+
+#define GLLoggingPageImpression @"GLLoggingPageImpression"
+#define GLLoggingTrackedEvents @"GLLoggingTrackedEvents"
+#define GLLoggingEventName @"GLLoggingEventName"
+#define GLLoggingEventSelectorName @"GLLoggingEventSelectorName"
+#define GLLoggingEventHandlerBlock @"GLLoggingEventHandlerBlock"
+#define GLLoggingEventHandlerBlock2 @"GLLoggingEventHandlerBlock2"
+
+@interface GLLogging : NSObject
+
++ (void)setupWithConfiguration:(NSDictionary *)configs;
+
+@end
